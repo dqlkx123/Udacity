da0e13e6cb30ab2937f53bdbd73f4091874ba7e7
Liu, Kaixi <kaixliu@ebay.com>
2017-08-02 10:14:35 -0700

    finish detect spam

--- Detect Spam/Code/detectSpam.py
+++ Detect Spam/Code/detectSpam.py
@@ -20,17 +20,39 @@ count_vector = CountVectorizer()
 count_vector.fit(documents)
 doc_array = count_vector.transform(documents).toarray()
 
-#print(doc_array)
+
 frequency_matrix = pd.DataFrame(doc_array,
                                 columns = count_vector.get_feature_names())
-#print(frequency_matrix)
-
+print(frequency_matrix)
+print(count_vector.transform(documents))
 #divide data into train and setting
 X_train, X_test, y_train, y_test = train_test_split(df['sms_message'],
                                                     df['label'],
                                                     random_state=1)
-print('Number of rows in the total set: {}'.format(df.shape[0]))
-print('Number of rows in the training set: {}'.format(X_train.shape[0]))
-print('Number of rows in the test set: {}'.format(X_test.shape[0]))
+#print('Number of rows in the total set: {}'.format(df.shape[0]))
+#print('Number of rows in the training set: {}'.format(X_train.shape[0]))
+#print('Number of rows in the test set: {}'.format(X_test.shape[0]))
 
 #print(df.head())
+
+# Instantiate the CountVectorizer method
+count_vector = CountVectorizer()
+
+# Fit the training data and then return the matrix
+training_data = count_vector.fit_transform(X_train)
+
+# Transform testing data and return the matrix. Note we are not fitting the testing data into the CountVectorizer()
+testing_data = count_vector.transform(X_test)
+
+from sklearn.naive_bayes import MultinomialNB
+naive_bayes = MultinomialNB()
+naive_bayes.fit(training_data, y_train)
+
+
+predictions = naive_bayes.predict(testing_data)
+print(predictions)
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+print('Accuracy score: ', format(accuracy_score(y_test, predictions)))
+print('Precision score: ', format(precision_score(y_test, predictions)))
+print('Recall score: ', format(recall_score(y_test, predictions)))
+print('F1 score: ', format(f1_score(y_test, predictions)))
